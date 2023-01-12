0afadff9548446f629352a497f3a21568efd19b4
Luis André Ramos das Chagas Júnior <92380986+AndreCh90@users.noreply.github.com>
2023-01-12 20:38:07 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -1,43 +1,45 @@
-function verificaSeOChutePossueValorValido(chute) {
+function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>valor inválido</div>'
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
         return
     }
 
-    if (numeroMaiorOuMenorQueOPermitido(numero))
-        elementoChute.innerHTML += `<idv>valor inválido! Diga um número entre ${menorValor} e ${maiorValor}</div>`
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
         return
+    }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
-            <h2>Parabéns, você acertou o número secreto!</h2>
-            <h3> ${numeroSecreto}</h3>
-            <button id="jogar-novamente" class="btn-jogar-novamente"></button>
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
-    }else if (numero > numeroSecreto){
+    } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-dow-long">
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
-    }else {
+    } else {
         elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-up-long">
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
-
 }
 
 function chuteForInvalido(numero) {
     return Number.isNaN(numero)
 }
 
-function numeroMaiorOuMenorQueOPermitido(numero) {
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
 
 document.body.addEventListener('click', e => {
-    if (e.target.id == 'jogar-novamente'){
-        window.location.reload()//*Recarrega a página atual e reinicia o jogo.
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
     }
 })
